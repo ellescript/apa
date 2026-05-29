a683126404f2e288920539510d01986a9ea28e41
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2026-05-29 13:58:41 -0600

    adjustments to build

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'index.js',
+
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'esm',
+            sourcemap: true
+        },
+        {
+            file: 'dist/index.cjs',
+            format: 'cjs',
+            sourcemap: true
+        }
+    ],
+
+    plugins: [
+        resolve(),
+        commonjs()
+    ],
+
+    external: [
+        '@ellescript/collecty'
+    ]
+};
